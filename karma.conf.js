02f19170db4a0d8aa1da548d8094983da80dfbd9
bhavin-patel <bhavin.patel@pubmatic.com>
2022-02-13 00:18:15 +0530

    Added pawn Simulator

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -21,24 +21,37 @@ module.exports = function (config) {
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+    // jasmineHtmlReporter: {
+    //   suppressAll: true // removes the duplicated traces
+    // },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      'report-config': {
+        html: { subdir: 'html' }
+      },
+      thresholds: {
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100
+        }
+      },
+      fixWebpackSourcePaths: true
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ngv'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true
   });
 };
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
+    path: 'simulator',
+    loadChildren: () => import('./pawn-game/pawn-game.module').then( m => m.PawnGameModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'simulator',
     pathMatch: 'full'
   },
 ];
